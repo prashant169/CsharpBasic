cdd8be30c9a6e99285905ad063ab3573e7a972ab
prashant169 <prashantphad2018@gmail.com>
2023-03-01 11:39:08 +0530

    Added: User input And Addition

---
+++ UserInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("hello " + name);
+            Console.WriteLine("Enter Two No");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("addition :" + (a + b)); 
+            Console.ReadLine();
+        }
+    }
+}
